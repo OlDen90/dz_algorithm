1127173578a2be51ff572075b606e99e4975633b
OlDen <olden90@mail.ru>
2022-07-20 21:13:48 +0300

    changed files in dz_2

--- Oleinik_Denis_dz_2/task_2_3.py
+++ Oleinik_Denis_dz_2/task_2_3.py
@@ -5,10 +5,25 @@
 """
 # https://drive.google.com/file/d/19ki-RLtNiuxUS2FVoIt2Tmt5R5k_YdY0/view?usp=sharing
 
-n = int(input('Введите число: '))
-m = 0
-while n > 0:
-    m = m * 10 + n % 10
-    n = n // 10
-print(m)
 
+def reverse(string):
+    if len(string) == 0:
+        return string
+    else:
+        return reverse(string[1:]) + string[0]
+a = str(input('Введите строку: '))
+print(reverse(a))
+
+# -----------------------------------
+
+# n = int(input('Введите число: '))
+# m = 0
+# while n > 0:
+#     m = m * 10 + n % 10
+#     n = n // 10
+# print(m)
+
+# -----------------------------------
+
+# a = input('Введите число: ')
+# print(a[::-1])
